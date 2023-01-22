81eb4c08aa18b582f14c83fafec5a4fc04b484fd
Buziu <mateusz.buzalski@gmail.com>
2023-01-22 19:51:59 +0100

    dodanie ruchu komputera

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage(
-  "Zagrałem " + playerMove + "! Jeśli Twój ruch to nożyce, to wygrywasz!"
-);
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+elseif(randomNumber == 2){
+    computerMove = 'papier';
+}
+elseif(randomNumber == 3){
+    computerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + computerMove);
