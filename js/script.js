e5811b3a35db02d6b6981e9cb09672a9143b2fe6
Buziu <mateusz.buzalski@gmail.com>
2023-01-23 17:27:05 +0100

    dodanie funkcji getMoveName

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
+function getMoveName(argMoveId) {
+  if (argMoveId == 1) {
+    return "kamień";
+  } else if (argMoveId == 2) {
+    return "papier";
+  } else if (argMoveId == 3) {
+    return "nożyce";
+  }
+  printMessage("Nie znam ruchu o id " + argMoveId + ".");
+  return "nieznany ruch";
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log("Wylosowana liczba to: " + randomNumber);
 
-let computerMove = "nieznany ruch";
+let computerMove = getMoveName(randomNumber);
 
-if (randomNumber == 1) {
+/* if (randomNumber == 1) {
   computerMove = "kamień";
 } else if (randomNumber == 2) {
   computerMove = "papier";
 } else if (randomNumber == 3) {
   computerMove = "nożyce";
-}
+} */
 printMessage("Mój ruch to: " + computerMove);
 
 let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
@@ -26,6 +38,7 @@ if (playerInput == "1") {
 } else if (playerInput == 3) {
   playerMove = "nożyce";
 }
+
 printMessage("Twój ruch to: " + playerMove);
 
 if (computerMove == "kamień" && playerMove == "papier") {
